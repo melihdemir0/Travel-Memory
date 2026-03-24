58946041325e9b9c0f1d4181fc497921e6b67a99
melihdemir0 <melihdemirr0@gmail.com>
2026-03-24 11:55:32 +0300

    biome fix

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
 import "../styles/globals.css";
-import App from "./App";
 import { I18nProvider } from "../context/I18nContext";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
